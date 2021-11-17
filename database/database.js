3ed83cbdd02796effb83053af73774cb5fd2b0da
EmilyRodrigues17 <emilyrodrigues17@hotmail.com>
2021-11-17 18:32:27 -0300

    feat: add database connection

---
+++ database/database.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const { Sequelize } = require("sequelize")
+const databaseName = process.env.DATABASE
+const hostName = process.env.HOST
+const password = process.env.PASSWORD 
+
+const connection = new Sequelize(databaseName, 'root', password,{
+    host: hostName,
+    dialect: 'mysql'
+})
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+// Database
+// Estrutura promise
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgError) => {
+        console.log(msgError)
+    })
 
 // Modo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// Rotas
 app.get("/", (req, res) => {
     
     res.render("index");
@@ -12,6 +27,12 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar")
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    res.send(`Formulario recebido! Titulo: ${titulo} Descricao: ${descricao}`);
 });
 
 app.listen(3333, () => {
