968bdc0a1b873a1a56fcd9c0b7f820d3a14f94f6
EmilyRodrigues17 <emilyrodrigues17@hotmail.com>
2021-11-18 09:11:39 -0300

    feat: add view perguntas

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ app.use(bodyParser.json());
 
 // Rotas
 app.get("/", (req, res) => {
-    res.render("index");
+    pergunta.findAll({raw: true}).then((perguntas) => {
+        res.render("index", {
+            perguntas: perguntas
+        });
+    });    
 });
 
 app.get("/perguntar", (req, res) => {
